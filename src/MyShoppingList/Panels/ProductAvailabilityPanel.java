b514d3847647254856b160c8d54c8abc04e97252
mlaykj99 <joshgreenwell779@yahoo.com>
2014-09-27 11:52:58 -0500

    Almost there!!!

--- src/MyShoppingList/Panels/ProductAvailabilityPanel.java
+++ src/MyShoppingList/Panels/ProductAvailabilityPanel.java
@@ -1,12 +1,16 @@
 package MyShoppingList.Panels;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -24,6 +28,14 @@ public class ProductAvailabilityPanel extends JPanel implements ActionListener ,
 	private JButton btnBack;
 	private JLabel lblDirectionsimage;
 	private Util util = new Util();
+	private JPanel panel;
+	private JTextArea textArea_1;
+	private JTextArea textArea_2;
+	private JPanel panel_1;
+	
+	private String[] items;
+	private String[] prices;
+	private String[] quantities;
 	
 	public ProductAvailabilityPanel(Driver d)
 	{
@@ -31,9 +43,7 @@ public class ProductAvailabilityPanel extends JPanel implements ActionListener ,
 		setLayout(null);
 		driver = d;
 		
-		lblDirectionsimage = new JLabel("");
-		lblDirectionsimage.setBounds(0, 0, 800, 600);
-		add(lblDirectionsimage);
+		
 		
 		setVisible(false);
 		setBounds(0,0,800,600);
@@ -48,6 +58,12 @@ public class ProductAvailabilityPanel extends JPanel implements ActionListener ,
 		btnDirections.setActionCommand("directions");
 		add(btnDirections);
 		
+		btnBack = new JButton("Back");
+		btnBack.setBounds(347, 526, 97, 25);
+		btnBack.addActionListener(this);
+		btnBack.setActionCommand("back");
+		add(btnBack);
+		
 		lblPhoneNumberaddress = new JLabel("Phone Number/Address");
 		lblPhoneNumberaddress.setBounds(95, 119, 173, 16);
 		add(lblPhoneNumberaddress);
@@ -56,30 +72,56 @@ public class ProductAvailabilityPanel extends JPanel implements ActionListener ,
 		scrollPane.setBounds(95, 148, 598, 365);
 		add(scrollPane);
 		
+		panel_1 = new JPanel();
+		panel_1.setBackground(Color.GRAY);
+		scrollPane.setViewportView(panel_1);
+		panel_1.setLayout(null);
+		
 		textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
+		textArea.setBounds(0, 0, 291, 363);
+		panel_1.add(textArea);
 		textArea.setEditable(false);
 		
-		btnBack = new JButton("Back");
-		btnBack.setBounds(347, 526, 97, 25);
-		btnBack.addActionListener(this);
-		btnBack.setActionCommand("back");
-		add(btnBack);
+		textArea_1 = new JTextArea();
+		textArea_1.setBounds(295, 0, 150, 363);
+		panel_1.add(textArea_1);
+		
+		textArea_2 = new JTextArea();
+		textArea_2.setBounds(449, 0, 147, 363);
+		panel_1.add(textArea_2);
+		
+		panel = new JPanel();
+		panel.setOpaque(false);
+		panel.setBounds(0, 0, 800, 600);
+		add(panel);
 		
 		lblDirectionsimage = new JLabel("");
-		lblDirectionsimage.setBounds(0, 0, 800, 600);
-		add(lblDirectionsimage);
-		lblDirectionsimage.addMouseListener(this);
+		panel.add(lblDirectionsimage);
+		lblDirectionsimage.setBackground(Color.BLACK);
+		
 	}
 	
 	public void setLabelsAndProducts(String name, String addressPhoneNumber, String[] products)
 	{
 		lblName.setText(name);
 		lblPhoneNumberaddress.setText(addressPhoneNumber);
+		String[][] prodInfo = util.getProductInfo(driver.getListPanel().getProducts(), driver.getStoreResults().getSelectedStoreId());
+		items = prodInfo[0];
+		prices = prodInfo[1];
+		quantities = prodInfo[2];
 		for(int i = 0; i < products.length; i++)
 		{
-			textArea.setText(textArea.getText()+products[i]+"\n");
+			textArea.setText(textArea.getText()+items[i]+"\n");
 		}
+		for(int i = 0; i < products.length; i++)
+		{
+			textArea_1.setText(textArea.getText()+prices[i]+"\n");
+		}
+		for(int i = 0; i < products.length; i++)
+		{
+			textArea_2.setText(textArea.getText()+quantities[i]+"\n");
+		}
+		
 	}
 
 	@Override
@@ -87,7 +129,11 @@ public class ProductAvailabilityPanel extends JPanel implements ActionListener ,
 	{
 		if(e.getActionCommand().equals("directions"))
 		{
+			//JFrame dir = new JFrame();
+			//dir.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//dir.setBounds(0, 0, 500, 300);
 			lblDirectionsimage.setIcon(util.getDirections(driver.getAddressPanel().getAddress(), lblName.getText()));
+			lblDirectionsimage.setVisible(true);
 		}
 		else if(e.getActionCommand().equals("back"))
 		{
--- src/MyShoppingList/Panels/StoreResultsPanel.java
+++ src/MyShoppingList/Panels/StoreResultsPanel.java
@@ -21,8 +21,9 @@ public class StoreResultsPanel extends JPanel implements MouseListener , ActionL
 	private Driver driver;
 	private DefaultListModel stores;
 	private JScrollPane scrollPane;
-	private JLabel[] labels = new JLabel[10];
+	private JLabel[] labels = new JLabel[30];
 	private String[][] rawStores;
+	private String selectedStoreId;
 	
 	//Labels
 	private JLabel label;
@@ -35,6 +36,29 @@ public class StoreResultsPanel extends JPanel implements MouseListener , ActionL
 	private JLabel label_7;
 	private JLabel label_8;
 	private JLabel label_9;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JPanel panel_3;
+	private JLabel label_10;
+	private JLabel label_11;
+	private JLabel label_12;
+	private JLabel label_13;
+	private JLabel label_14;
+	private JLabel label_15;
+	private JLabel label_16;
+	private JLabel label_17;
+	private JLabel label_18;
+	private JLabel label_19;
+	private JLabel label_20;
+	private JLabel label_21;
+	private JLabel label_22;
+	private JLabel label_23;
+	private JLabel label_24;
+	private JLabel label_25;
+	private JLabel label_26;
+	private JLabel label_27;
+	private JLabel label_28;
+	private JLabel label_29;
 	
 	public StoreResultsPanel(Driver d)
 	{
@@ -51,65 +75,180 @@ public class StoreResultsPanel extends JPanel implements MouseListener , ActionL
 		add(panel);
 		panel.setLayout(null);
 		
+		panel_1 = new JPanel();
+		panel_1.setBounds(0, 0, 341, 253);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
 		label_9 = new JLabel("");
-		label_9.setBounds(12, 13, 648, 16);
-		panel.add(label_9);
+		label_9.setBounds(10, 10, 315, 16);
+		panel_1.add(label_9);
 		labels[0] = label_9;
-		label_9.addMouseListener(this);
 		
 		label = new JLabel("");
-		label.setBounds(12, 36, 648, 16);
-		panel.add(label);
+		label.setBounds(10, 30, 315, 16);
+		panel_1.add(label);
 		labels[1] = label;
-		label.addMouseListener(this);
 		
 		label_1 = new JLabel("");
-		label_1.setBounds(12, 59, 648, 16);
-		panel.add(label_1);
+		label_1.setBounds(10, 50, 315, 16);
+		panel_1.add(label_1);
 		labels[2] = label_1;
-		label_1.addMouseListener(this);
 		
 		label_2 = new JLabel("");
-		label_2.setBounds(12, 82, 648, 16);
-		panel.add(label_2);
+		label_2.setBounds(10, 70, 315, 16);
+		panel_1.add(label_2);
 		labels[3] = label_2;
-		label_2.addMouseListener(this);
 		
 		label_3 = new JLabel("");
-		label_3.setBounds(12, 106, 648, 16);
-		panel.add(label_3);
+		label_3.setBounds(10, 90, 315, 16);
+		panel_1.add(label_3);
 		labels[4] = label_3;
-		label_3.addMouseListener(this);
 		
 		label_4 = new JLabel("");
-		label_4.setBounds(12, 128, 648, 16);
-		panel.add(label_4);
+		label_4.setBounds(10, 110, 315, 16);
+		panel_1.add(label_4);
 		labels[5] = label_4;
-		label_4.addMouseListener(this);
 		
 		label_5 = new JLabel("");
-		label_5.setBounds(12, 150, 648, 16);
-		panel.add(label_5);
+		label_5.setBounds(10, 130, 315, 16);
+		panel_1.add(label_5);
 		labels[6] = label_5;
-		label_5.addMouseListener(this);
 		
 		label_6 = new JLabel("");
-		label_6.setBounds(12, 172, 648, 16);
-		panel.add(label_6);
+		label_6.setBounds(10, 150, 315, 16);
+		panel_1.add(label_6);
 		labels[7] = label_6;
-		label_6.addMouseListener(this);
 		
 		label_7 = new JLabel("");
-		label_7.setBounds(12, 194, 648, 16);
-		panel.add(label_7);
+		label_7.setBounds(10, 170, 315, 16);
+		panel_1.add(label_7);
 		labels[8] = label_7;
-		label_7.addMouseListener(this);
 		
 		label_8 = new JLabel("");
-		label_8.setBounds(12, 216, 648, 16);
-		panel.add(label_8);
+		label_8.setBounds(10, 190, 315, 16);
+		panel_1.add(label_8);
 		labels[9] = label_8;
 		label_8.addMouseListener(this);
+		label_7.addMouseListener(this);
+		label_6.addMouseListener(this);
+		label_5.addMouseListener(this);
+		label_4.addMouseListener(this);
+		label_3.addMouseListener(this);
+		label_2.addMouseListener(this);
+		label_1.addMouseListener(this);
+		label.addMouseListener(this);
+		label_9.addMouseListener(this);
+		
+		panel_2 = new JPanel();
+		panel_2.setBounds(339, 0, 202, 253);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		label_10 = new JLabel("");
+		label_10.setBounds(10, 10, 180, 16);
+		panel_2.add(label_10);
+		labels[10] = label_10;
+		
+		label_11 = new JLabel("");
+		label_11.setBounds(10, 30, 180, 16);
+		panel_2.add(label_11);
+		labels[11] = label_11;
+		
+		label_12 = new JLabel("");
+		label_12.setBounds(10, 50, 180, 16);
+		panel_2.add(label_12);
+		labels[12] = label_12;
+		
+		label_13 = new JLabel("");
+		label_13.setBounds(10, 70, 180, 16);
+		panel_2.add(label_13);
+		labels[13] = label_13;
+		
+		label_14 = new JLabel("");
+		label_14.setBounds(10, 90, 180, 16);
+		panel_2.add(label_14);
+		labels[14] = label_14;
+		
+		label_15 = new JLabel("");
+		label_15.setBounds(10, 110, 180, 16);
+		panel_2.add(label_15);
+		labels[15] = label_15;
+		
+		label_16 = new JLabel("");
+		label_16.setBounds(10, 130, 180, 16);
+		panel_2.add(label_16);
+		labels[16] = label_16;
+		
+		label_17 = new JLabel("");
+		label_17.setBounds(10, 150, 180, 16);
+		panel_2.add(label_17);
+		labels[17] = label_17;
+		
+		label_18 = new JLabel("");
+		label_18.setBounds(10, 170, 180, 16);
+		panel_2.add(label_18);
+		labels[18] = label_18;
+		
+		label_19 = new JLabel("");
+		label_19.setBounds(10, 190, 180, 16);
+		panel_2.add(label_19);
+		labels[19] = label_19;
+		
+		panel_3 = new JPanel();
+		panel_3.setBounds(540, 0, 132, 253);
+		panel.add(panel_3);
+		panel_3.setLayout(null);
+		
+		label_20 = new JLabel("");
+		label_20.setBounds(10, 10, 110, 16);
+		panel_3.add(label_20);
+		labels[20] = label_20;
+		
+		label_21 = new JLabel("");
+		label_21.setBounds(10, 30, 110, 16);
+		panel_3.add(label_21);
+		labels[21] = label_21;
+		
+		label_22 = new JLabel("");
+		label_22.setBounds(10, 50, 110, 16);
+		panel_3.add(label_22);
+		labels[22] = label_22;
+		
+		label_23 = new JLabel("");
+		label_23.setBounds(10, 70, 110, 16);
+		panel_3.add(label_23);
+		labels[23] = label_23;
+		
+		label_24 = new JLabel("");
+		label_24.setBounds(10, 90, 110, 16);
+		panel_3.add(label_24);
+		labels[24] = label_24;
+		
+		label_25 = new JLabel("");
+		label_25.setBounds(10, 110, 110, 16);
+		panel_3.add(label_25);
+		labels[25] = label_25;
+		
+		label_26 = new JLabel("");
+		label_26.setBounds(10, 130, 110, 16);
+		panel_3.add(label_26);
+		labels[26] = label_26;
+		
+		label_27 = new JLabel("");
+		label_27.setBounds(10, 150, 110, 16);
+		panel_3.add(label_27);
+		labels[27] = label_27;
+		
+		label_28 = new JLabel("");
+		label_28.setBounds(10, 170, 110, 16);
+		panel_3.add(label_28);
+		labels[28] = label_28;
+		
+		label_29 = new JLabel("");
+		label_29.setBounds(10, 190, 110, 16);
+		panel_3.add(label_29);
+		labels[29] = label_29;
 		
 		JButton btnBack = new JButton("Back");
 		btnBack.setBounds(353, 485, 97, 25);
@@ -118,19 +257,29 @@ public class StoreResultsPanel extends JPanel implements MouseListener , ActionL
 		add(btnBack);
 	}
 	
+	public String getSelectedStoreId(){return selectedStoreId;}
+	
 	public void setUp()
 	{
 		Util util = new Util(driver);
 		String[] storeInfo;
 		String addition = "";
 		rawStores = util.closestStores(driver.getAddressPanel().getAddress(),driver.getAddressPanel().getRange());
-		storeInfo = new String[rawStores.length];
+		storeInfo = new String[rawStores.length*3];
 
 		
 		
 		for(int i = 0; i < rawStores.length; i++)
 		{
-				storeInfo[i] = rawStores[i][2] + "           " + rawStores[i][3] + "                             " + rawStores[i][4];
+				storeInfo[i] = rawStores[i][2];
+		}
+		for(int i = 0; i < rawStores.length; i++)
+		{
+				storeInfo[i+10] = rawStores[i][3];
+		}
+		for(int i = 0; i < rawStores.length; i++)
+		{
+				storeInfo[i+20] = rawStores[i][4];
 		}
 		
 		for(int i = 0; i < storeInfo.length; i++)
@@ -147,42 +296,52 @@ public class StoreResultsPanel extends JPanel implements MouseListener , ActionL
 		if(e.getSource().equals(label))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[1][2],rawStores[1][3] + " | " + rawStores[1][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[1][1];
 		}
 		else if(e.getSource().equals(label_1))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[2][2],rawStores[2][3] + " | " + rawStores[2][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[2][1];
 		}
 		else if(e.getSource().equals(label_2))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[3][2],rawStores[3][3] + " | " + rawStores[3][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[3][1];
 		}
 		else if(e.getSource().equals(label_3))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[4][2],rawStores[4][3] + " | " + rawStores[4][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[4][1];
 		}
 		else if(e.getSource().equals(label_4))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[5][2],rawStores[5][3] + " | " + rawStores[5][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[5][1];
 		}
 		else if(e.getSource().equals(label_5))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[6][2],rawStores[6][3] + " | " + rawStores[6][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[6][1];
 		}
 		else if(e.getSource().equals(label_6))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[7][2],rawStores[7][3] + " | " + rawStores[7][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[7][1];
 		}
 		else if(e.getSource().equals(label_7))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[8][2],rawStores[8][3] + " | " + rawStores[8][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[8][1];
 		}
 		else if(e.getSource().equals(label_8))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[9][2],rawStores[9][3] + " | " + rawStores[9][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[9][1];
 		}
 		else if(e.getSource().equals(label_9))
 		{
 			driver.getProductAvailability().setLabelsAndProducts(rawStores[0][2],rawStores[0][3] + " | " + rawStores[0][4], driver.getListPanel().getProducts());
+			selectedStoreId = rawStores[0][1];
 		}
 	}
 
